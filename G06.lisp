64b11829e58af70df15b3b71444478687f4154e6
pcostam <pcosta.margarida@gmail.com>
2018-11-24 21:35:06 +0000

    acrescento restricao fazer refeicao

--- G06.lisp
+++ G06.lisp
@@ -8,7 +8,7 @@
   )
 
 (defun csp-inicial(problema)
-  (let ((var_values (make-hash-table)) (t_end NIL) (t_init NIL) (t_tarefa NIL) )
+  (let ((var_values (make-hash-table)) (t_end NIL) (t_init NIL) (t_tarefa NIL) (tarefa NIL))
     (dotimes (i (list-length problema))
       (let  ((el (elt problema i))) 
       
@@ -19,6 +19,7 @@
        ( setq t_end (aref a 0 3))
        (setq t_init (aref a 0 2))
        (setq t_tarefa (- t_end t_init))
+       (setf tarefa a)
        (setf (gethash a var_values) t_tarefa)
         )
       
@@ -31,27 +32,62 @@
   (make-csp       :variables var_values
             :max_lim 480 
             :not_assigned problema
-            :assignments ())))
+            :assignments (make-hash-table))))
                  
     ini)
    )
-)
-(defun operadores(estado)
+  )
+
+(defun fazer-refeicao(tarefa)
+  (let ((nova_tarefa NIL) (novo_t_end 0 ) (novo_t_inicio 0) (tarefas NIL) (t_init (elt tarefa 2))
+        (t_tarefa 0)  (t_end (elt tarefa 3)))
+                                                                                  
+                                                                                  
+    (setq t_tarefa (- t_end t_init))
+    (setq novo_t_end (+ t_init 240))
+    (setf nova_tarefa tarefa)
+    (setf (elt nova_tarefa 3) novo_t_end)
+    
+    (setq tarefas (append tarefas nova_tarefa))
+    (write nova_tarefa)
+    
+    (setq novo_t_inicio (+ novo_t_end 40))
+    (setq novo_t_end (+ novo_t_inicio (- t_tarefa 4) ))
+    (setf nova_tarefa tarefa)
+    (setf (elt nova_tarefa 3) novo_t_end)
+    (setf (elt nova_tarefa 2) novo_t_inicio)
+    (write nova_tarefa)
+    (setq tarefas (append tarefas nova_tarefa))
+    
+    
+
+    )
   
-  (let ((suc_all NIL))
-    (dotimes (i (list-length csp-assignments estado))
-      ;; Se nao comecar em L1
-      (if (not(equal((car(elt 0 (estado i))) 'L1)))
-          ()
+
+)                   
+
+
+(defun operadores(estado)
+      (let ((suc_all NIL) (tarefa NIL))
+    (dotimes (i (list-length (csp-not_assigned estado)))
+      
       )
+      
      
+  
+       
       
     )
-         
-        
-    )  
-  )
+      
+
+   
+      
+ 
+       
+      
 )
+         
+
 
 
 (defun custo(estado)
@@ -65,8 +101,10 @@
   )
 
 
-(defun duracao_tarefa()
-  )
+(defun duracao_tarefa(tarefa)
+  
+      
+)
 
 (defun heuristica(estado)
   )
