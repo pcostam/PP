21faabc81a2a55e94eab3bdd8796305ebcc26f43
pcostam <pcosta.margarida@gmail.com>
2018-11-26 23:17:57 +0000

    tive que mudar estrutura de variaveis para tuplo de tuplos (2º elemento é numero

--- G06.lisp
+++ G06.lisp
@@ -1,5 +1,28 @@
 (in-package :user)
 
+
+(defun qsort (L)
+  (cond
+    ((null L) nil)
+    (t
+      (append
+        (qsort (list< (car L) (cdr L)))
+        (cons (car L) nil) 
+        (qsort (list>= (car L) (cdr L)))))))
+
+(defun list< (a b)
+  (cond
+    ((or (null a) (null b)) nil)
+    ((< (elt a 2) (elt (car b) 2)) (list< a (cdr b)))
+    (t (cons (car b) (list< a (cdr b))))))
+
+(defun list>= (a b)
+  (cond
+    ((or (null a) (null b)) nil)
+    ((>= (elt a 2) (elt (car b) 2)) (list>= a (cdr b)))
+   (t (cons (car b) (list>= a (cdr b))))))
+
+
 (defstruct csp
   variables
   max_lim
@@ -7,25 +30,24 @@
   )
 
 (defun csp-inicial(problema)
-  (let ((var_values (make-hash-table)) (t_end NIL) (t_init NIL) (t_tarefa NIL) (tarefa NIL))
-    (dotimes (i (list-length problema))
-      (let  ((el (elt problema i))) 
-      
+  (setf problema (qsort problema))
+  (let ((var_values NIL) (t_end NIL) (t_init NIL) (t_tarefa NIL) (tarefa NIL)) 
+    
+    (dotimes (i (list-length problema))      
       (let(
-            (tarefa (make-array '(1 4) :initial-contents (list el)))
+            (tarefa (elt problema i))
  
            )
-       ( setq t_end (aref tarefa 0 3))
-       (setq t_init (aref tarefa 0 2))
+       (setq t_end (elt tarefa 3))
+       (setq t_init (elt tarefa 2))
        (setq t_tarefa (- t_end t_init))
-       (setf (gethash tarefa var_values) t_tarefa)
+       (print tarefa)
+       (setf var_values (append var_values (list (cons tarefa (list t_tarefa))))
         )
       
       
         ))
-  
-  (maphash #'(lambda (k v) (format t "~a => ~a~%" k v)) var_values)   
-  
+    
   (let ((ini
   (make-csp       :variables var_values
             :max_lim 480 
@@ -35,33 +57,6 @@
    )
   )
 
-(defun fazer-refeicao(tarefa)
-  (let ((nova_tarefa NIL) (novo_t_end 0 ) (novo_t_inicio 0) (tarefas NIL) (t_init (elt tarefa 2))
-        (t_tarefa 0)  (t_end (elt tarefa 3)))
-                                                                                  
-                                                                                  
-    (setq t_tarefa (- t_end t_init))
-    (setq novo_t_end (+ t_init 240))
-    (setf nova_tarefa tarefa)
-    (setf (elt nova_tarefa 3) novo_t_end)
-    
-    (setq tarefas (append tarefas nova_tarefa))
-    (write nova_tarefa)
-    
-    (setq novo_t_inicio (+ novo_t_end 40))
-    (setq novo_t_end (+ novo_t_inicio (- t_tarefa 4) ))
-    (setf nova_tarefa tarefa)
-    (setf (elt nova_tarefa 3) novo_t_end)
-    (setf (elt nova_tarefa 2) novo_t_inicio)
-    (write nova_tarefa)
-    (setq tarefas (append tarefas nova_tarefa))
-    
-    
-
-    )
-  
-
-)                   
 
 (defun duracao_tarefa(tarefa)
   
@@ -73,67 +68,21 @@
 )
 
 
-(defun operadores(estado)
-      (let ((suc_all NIL) (tarefa NIL) (custo_tarefa NIL) (turno NIL) )
-        (dotimes (var_idx (list-length (csp-variables estado)))
-         (setf tarefa (elt (csp-variable estado) var_idx))
-         (loop for custo being the hash-values of (csp-assignments estado)
-             using (hash-key turno)
-               
-             do (format t "~&~A -> ~A" key value
-                  (if (equal(hash-table-size (csp-assignments estado)) 0 )
-                  (
-                
-                   )))
-               (if (< custo 8)
-              (
-               (setf (gethash turno (csp-assignments estado)) custo_tarefa)
-               (setf (gethash turno (csp-assignments estado)) (gethash turno (csp-assignments estado))     
-               (remhash turno (csp-assignments estado))
-                 )
-               
-               (  ;; Se nao comecar em L1
-        
-                   (setf custo_tarefa (duracao_tarefa tarefa))
-                   (if (not(equal(elt (elt (csp-variable estado) var_idx) 0) 'L1))
-                   (   
-                        (setf custo_tarefa (+ custo_tarefa 40))
-                        (setf (gethash tarefa (csp-assignments estado)) custo_tarefa))) 
-                 
-                 
-           ))
-          
-            
-        
-            
-          )
-     
-     )
-      
-     
-  
-       
-      
-    )
-      
-
-   
-      
- 
-       
-      
-)
-         
-
-
-
 (defun custo(estado)
+  (let ((custo 0))
+    (loop for custo_turno being the hash-values of (csp-assignments estado)
+               do (setq custo (+ custo custo_turno))
+   ) 
+custo )  )
+
+
+(defun objectivo(estado) 
+  (if (equal (csp-variables estado) NIL) T)
+  NIL
 )
-
-
-(defun objectivo(estado)
-  )
-
+  
+(defun estado(estado_inicial estado_final)
+  (equalp estado_inicial estado_final))
 
 
 (defun heuristica(estado)
