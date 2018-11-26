3e06fa0f30f719361dc710dcdad0d47f40416ede
pcostam <pcosta.margarida@gmail.com>
2018-11-26 19:53:17 +0000

    nao funciona mas olha

--- G06.lisp
+++ G06.lisp
@@ -3,7 +3,6 @@
 (defstruct csp
   variables
   max_lim
-  not_assigned
   assignments
   )
 
@@ -13,14 +12,13 @@
       (let  ((el (elt problema i))) 
       
       (let(
-            (a (make-array '(1 4) :initial-contents (list el)))
+            (tarefa (make-array '(1 4) :initial-contents (list el)))
  
            )
-       ( setq t_end (aref a 0 3))
-       (setq t_init (aref a 0 2))
+       ( setq t_end (aref tarefa 0 3))
+       (setq t_init (aref tarefa 0 2))
        (setq t_tarefa (- t_end t_init))
-       (setf tarefa a)
-       (setf (gethash a var_values) t_tarefa)
+       (setf (gethash tarefa var_values) t_tarefa)
         )
       
       
@@ -31,7 +29,6 @@
   (let ((ini
   (make-csp       :variables var_values
             :max_lim 480 
-            :not_assigned problema
             :assignments (make-hash-table))))
                  
     ini)
@@ -66,12 +63,52 @@
 
 )                   
 
+(defun duracao_tarefa(tarefa)
+  
+  (setq t_end (aref a 0 3))
+  (setq t_init (aref a 0 2))
+  (setq t_tarefa (- t_end t_init))
+  t_tarefa
+      
+)
+
 
 (defun operadores(estado)
-      (let ((suc_all NIL) (tarefa NIL))
-    (dotimes (i (list-length (csp-not_assigned estado)))
-      
-      )
+      (let ((suc_all NIL) (tarefa NIL) (custo_tarefa NIL) (turno NIL) )
+        (dotimes (var_idx (list-length (csp-variables estado)))
+         (setf tarefa (elt (csp-variable estado) var_idx))
+         (loop for custo being the hash-values of (csp-assignments estado)
+             using (hash-key turno)
+               
+             do (format t "~&~A -> ~A" key value
+                  (if (equal(hash-table-size (csp-assignments estado)) 0 )
+                  (
+                
+                   )))
+               (if (< custo 8)
+              (
+               (setf (gethash turno (csp-assignments estado)) custo_tarefa)
+               (setf (gethash turno (csp-assignments estado)) (gethash turno (csp-assignments estado))     
+               (remhash turno (csp-assignments estado))
+                 )
+               
+               (  ;; Se nao comecar em L1
+        
+                   (setf custo_tarefa (duracao_tarefa tarefa))
+                   (if (not(equal(elt (elt (csp-variable estado) var_idx) 0) 'L1))
+                   (   
+                        (setf custo_tarefa (+ custo_tarefa 40))
+                        (setf (gethash tarefa (csp-assignments estado)) custo_tarefa))) 
+                 
+                 
+           ))
+          
+            
+        
+            
+          )
+     
+     )
       
      
   
@@ -93,18 +130,11 @@
 (defun custo(estado)
 )
 
-(defun no-overlap-constraints(estado)
-    
-  )
 
 (defun objectivo(estado)
   )
 
 
-(defun duracao_tarefa(tarefa)
-  
-      
-)
 
 (defun heuristica(estado)
   )
